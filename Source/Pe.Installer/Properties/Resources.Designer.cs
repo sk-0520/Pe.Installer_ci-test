3b96d9a2428b92e486bf6e8da0df4226a213aab3
sk <content.type.text.net@gmail.com>
2022-10-09 15:12:03 +0900

    文字がおかしい

--- Source/Pe.Installer/Properties/Resources.Designer.cs
+++ Source/Pe.Installer/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Pe.Installer.Properties {
     // または Visual Studio のようなツールを使用して自動生成されました。
     // メンバーを追加または削除するには、.ResX ファイルを編集して、/str オプションと共に
     // ResGen を実行し直すか、または VS プロジェクトをビルドし直します。
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -279,7 +279,7 @@ namespace Pe.Installer.Properties {
         }
         
         /// <summary>
-        ///   委譲 に類似しているローカライズされた文字列を検索します。
+        ///   異常 に類似しているローカライズされた文字列を検索します。
         /// </summary>
         internal static string String_LogKind_Error {
             get {
